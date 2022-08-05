6adbaa01a98ad57113e68c2f21b3dfd087975ec2
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-08-05 20:55:59 -0300

    feature: created the mocked login

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import { RoutPath } from "../src/types/routes";
 import Home from "../src/pages/Home";
 import Login from "./pages/Login";
+import { useState } from "react";
 
 const Router = () => {
+  const [logged, setLogged] = useState<boolean>(false);
+
   return (
     <Routes>
-      <Route path={RoutPath.HOME} element={<Home />} />
-
-      <Route path={RoutPath.LOGIN} element={<Login />} />
+      {logged ? (
+        <Route path={RoutPath.HOME} element={<Home setLogged={setLogged} />} />
+      ) : (
+        <Route
+          path={RoutPath.LOGIN}
+          element={<Login setLogged={setLogged} />}
+        />
+      )}
     </Routes>
   );
 };
