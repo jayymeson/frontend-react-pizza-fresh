1ebd522441c95904438ae588c7c6d24f428c7039
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-08-05 22:28:35 -0300

    fix: updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-// import { QueryClientProvider } from "@tanstack/react-query";
-// import { ThemeProvider } from "styled-components";
-// import theme from "../src/assets/styles/theme";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/assets/styles/theme";
 import { QueryClient } from "@tanstack/react-query";
 import Router from "./router";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <div>
-      {/* <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}> */}
-      <Toaster position="bottom-center" reverseOrder={false} />
-      <Router />
-      {/* </QueryClientProvider>
-    </ThemeProvider> */}
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Toaster position="bottom-center" reverseOrder={false} />
+          <Router />
+        </QueryClientProvider>
+      </ThemeProvider>
     </div>
   );
 };
