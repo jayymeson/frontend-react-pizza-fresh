e0a6136905010320ef2ff94b6620ec2bbffab9a1
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-08-17 21:22:04 -0300

    feature: created the funcionalite of categories

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,9 @@ import { Route, Routes, Navigate } from "react-router-dom";
 // import { RoutPath } from "../src/types/routes";
 import Home from "../src/pages/Home";
 import Login from "./pages/Login";
-import Settings from "./pages/Settings";
+import SettingsProducts from "./pages/SettingsProducts";
 import { useAuth } from "./contexts/auth";
+import SettingsCategories from "./pages/SettingsCategories";
 
 const Router = () => {
   const { logged } = useAuth();
@@ -13,7 +14,8 @@ const Router = () => {
       {logged ? (
         <>
           <Route path="/" element={<Home />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings/products" element={<SettingsProducts />} />
+          <Route path="/settings/categories" element={<SettingsCategories />} />
         </>
       ) : (
         <Route path="/login" element={<Login />} />
