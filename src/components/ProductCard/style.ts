e47a4a8137c7a16f4ea06c0fb298ba80ae8dd8f2
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-08-25 23:49:52 -0300

    feature: created the post and delete method

--- src/components/ProductCard/style.ts
+++ src/components/ProductCard/style.ts
@@ -7,8 +7,8 @@ interface CardContainerProps {
 
 export const CardContainer = styled.div<CardContainerProps>`
   ${({ theme }) => css`
-    width: 12rem;
-    height: 14.125rem;
+    width: 13.5rem;
+    height: 18.5rem;
     border-radius: 16px;
     background-color: ${theme.colors.baseBg2};
     display: flex;
@@ -26,6 +26,10 @@ export const CardContainer = styled.div<CardContainerProps>`
       width: 8rem;
       position: absolute;
       top: -3.3rem;
+    }
+
+    p{
+      margin-bottom: 1rem;
     }
   `}
 `;
